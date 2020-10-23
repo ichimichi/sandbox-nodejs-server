317a76d98cb9b6e4c8e1644e30988cac82b55105
Laribok Syiemlieh <ichimichilaribok@gmail.com>
2020-10-23 16:58:37 +0530

    updated api to use async await

--- src/res/item/item.router.js
+++ src/res/item/item.router.js
@@ -4,24 +4,23 @@ import { Item } from './item.model';
 const router = Router();
 
 router
-  .get('/', (req, res) => {
-    Item.find((err, items) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json(items);
-      }
-    });
+  .get('/', async (req, res) => {
+    try {
+      const items = await Item.find();
+      res.status(200).json(items);
+    } catch (e) {
+      console.error(e);
+      res.status(400).end();
+    }
   })
-  .post('/', (req, res) => {
-    const newItem = Item(req.body);
-    newItem.save((err, createdItem) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json(createdItem);
-      }
-    });
+  .post('/', async (req, res) => {
+    try {
+      const createdItem = await Item.create(req.body);
+      res.status(201).json(createdItem);
+    } catch (e) {
+      console.error(e);
+      res.status(400).end();
+    }
   });
 
 export default router;
--- src/res/user/user.router.js
+++ src/res/user/user.router.js
@@ -4,24 +4,23 @@ import { User } from './user.model';
 const router = Router();
 
 router
-  .get('/', (req, res) => {
-    User.find((err, users) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json(users);
-      }
-    });
+  .get('/', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (e) {
+      console.error(e);
+      res.status(400).end();
+    }
   })
-  .post('/', (req, res) => {
-    const newUser = User(req.body);
-    newUser.save((err, createdUser) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json(createdUser);
-      }
-    });
+  .post('/', async (req, res) => {
+    try {
+      const createdUser = await User.create(req.body);
+      res.status(201).json(createdUser);
+    } catch (e) {
+      console.error(e);
+      res.status(400).end();
+    }
   });
 
 export default router;
